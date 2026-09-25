8878a071aebb1778f995a974d78de504bae529cc
つがみこうすけ <tsugamichannel@gmail.com>
2026-09-25 21:09:37 +0900

    jsファイルを追加

---
+++ html-css/localStorage.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("loginForm");
+const emailInput = document.getElementById("yourEmail");
+const passwordInput = document.getElementById("password");
+const savedEmail = localStorage.getItem("email");
+const savedPassword = localStorage.getItem("password");
+
+emailInput.value = savedEmail;
+passwordInput.value = savedPassword;
+
+form.addEventListener("submit", () => {
+  localStorage.setItem("email", emailInput.value);
+  localStorage.setItem("password", passwordInput.value);
+});
